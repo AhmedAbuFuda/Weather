f65a26b10d63f90cae14ac1f6e963856348bcf90
Ahmed Abufoda <ahmed.abufoda70@gmail.com>
2024-03-11 17:32:48 +0200

    Make SplashScreen

---
+++ app/src/main/java/com/example/weather/splash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.weather.splash
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.MainActivity
+import com.example.weather.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+        }, 3500)
+    }
+}
